d3e1962c9b497bc0fd16a4bda55ad1b5458a8377
Kavyabhatnagar <kavya.bhatnagar.101201230@gmail.com>
2024-09-17 13:15:14 +0530

    updating routes2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 import Navbar from './Components/Navbar';
 import Main_container from './Components/Main_container';
@@ -7,66 +6,63 @@ import Error from './Components/Error';
 import About from './Components/About';
 import Cart from './Components/Cart';
 import Contact from './Components/Contact'; 
-import {lazy, Suspense} from 'react';
-import * as ReactDOM from "react-dom/client";
-import {createBrowserRouter,RouterProvider,Outlet} from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import RestrauntPage from './Components/RestrauntPage';
 import { Provider } from 'react-redux';
 import { AppStore } from './Utiles/AppStore';
 
-function Layout (){
+function Layout() {
   return (
- <Provider store={AppStore}>
-  <div className="App">
-  <Navbar/>
-  <Outlet/>
-  <Footer/>
-  </div>
-  </Provider> 
-
+    <Provider store={AppStore}>
+      <div className="App">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    </Provider>
   );
 }
-function App () {
- const AboutLoad = lazy(()=>{import('./Components/About')});
+
+function App() {
+  const AboutLoad = lazy(() => import('./Components/About'));
+  
   const Approuter = createBrowserRouter([
     {
-      // path: "/",
       path: "/FoodClub",
-      element:  <Layout/>,
-      errorElement:<Error/>,
-      children:[
+      element: <Layout />,
+      errorElement: <Error />,
+      children: [
         {
-          path: "/FoodClub",
-          element:<Main_container/>,
+          path: "/", // This will render the Main_container at the root of /FoodClub
+          element: <Main_container />,
         },
         {
-          path: "/FoodClub",
-          element:<status/>,
+          path: "About", // Relative path under /FoodClub
+          element: (
+            <Suspense fallback={<h1>Loading!!</h1>}>
+              <AboutLoad />
+            </Suspense>
+          ),
         },
         {
-          path: "/About",
-          element: <Suspense fallback={<h1>Loading!!</h1>}><About/></Suspense>
-          ,
+          path: "Contact", // Relative path under /FoodClub
+          element: <Contact />,
         },
         {
-          path: "/Contact",
-          element: <Contact/>,
+          path: "restraunts/:resId", // Relative path under /FoodClub
+          element: <RestrauntPage />,
         },
         {
-          path:'/restraunts/:resId',
-          element:<RestrauntPage/>
+          path: "Cart", // Relative path under /FoodClub
+          element: <Cart />,
         },
-        {
-          path:'/Cart',
-          element:<Cart/>
-        }
       ],
     }
-   
   ]);
-    return (<RouterProvider router={Approuter} />);
-  
- 
+
+  return <RouterProvider router={Approuter} />;
 }
 
 export default App;
