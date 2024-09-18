3edc1434d8a66cb10062391452b5ceb26c22df68
Kavyabhatnagar <kavya.bhatnagar.101201230@gmail.com>
2024-09-18 17:44:06 +0530

    12

--- src/App.js
+++ src/App.js
@@ -63,7 +63,7 @@ function App() {
   const basePath = process.env.REACT_APP_BASE_PATH;
   const Approuter = createBrowserRouter([
     {
-      path: basePath || '/', // Base path for all routes
+      path: basePath , // Base path for all routes
       element: <Layout />,
       errorElement: <Error />,
       children: [
